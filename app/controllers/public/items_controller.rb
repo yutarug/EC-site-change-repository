cbb318fba72853a9346acd06af7e0a94cf03444a
yutarug <nakamura.yuta.infratop@gmail.com>
2020-05-21 07:38:10 +0000

    modified2

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -4,4 +4,9 @@ class Public::ItemsController < ApplicationController
 		@end_user = current_end_user
 		@genres = Genre.all
 	end
+	def show
+		@item = Item.find(params[:id])
+		@end_user = current_end_user
+		@genres = Genre.all
+	end
 end
